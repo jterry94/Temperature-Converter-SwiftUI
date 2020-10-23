3a72f63299d59b8f2f14ca4d6b39137eb4c7b88d
Jeff Terry <terryj@iit.edu>
2020-10-23 11:57:17 -0500

    Initial Commit

---
+++ Temperature Converter SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Temperature Converter SwiftUI
+//
+//  Created by Jeff Terry on 10/23/20.
+//  Copyright © 2020 Jeff Terry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
